63a8866c96517cc73a14a732bd47fa46a6ba1b69
Martin Schröder <info@nordiskamarknadssystem.se>
2013-07-28 16:16:13 +0200

    ..

--- themes/livesite/init.js
+++ themes/livesite/init.js
@@ -1,7 +1,41 @@
-exports.init = function(x){
+var mustache = require("mustache");
+var j = require("jquery"); 
 
+var db = {}; 
+var templates = {};
+var server = {};
+exports.init = function(ctx){
+	server = ctx; 
+	db = ctx.db;
+	templates = ctx.templates; 
 }
 
-exports.render = function(path, args, session, callback){
-	callback("Welcome to LiveSite!"); 
+j.fn.outerHTML = function() {
+  return j('<div />').append(this.eq(0).clone()).html();
+};
+
+function Editable(x){
+	var res = j("<div class='editable' data-target='/edit_helper' data-object-type='"+x.type+"' data-object-id='"+x.id+"' data-property-name='"+x.name+"'>"+x.value+"</div>").outerHTML();
+	return res; 
+}
+
+exports.render = function(path, args, session, done){
+	var pages = server.pages; 
+	pages.get(path, function(error, page){
+		console.log("Render text page "+path); 
+		var html = session.render("root", {
+				title: j(page.title).text(),
+				head: "",
+				content: session.render("main_page", {
+					/*content: 	"<h2>"+Editable({type: "page", id: path, name: "title", value: page.title})+"</h2>"+
+										Editable({type: "page", id: path, name: "content", value: page.content})*/
+					content: Editable({type: "page", id: path, name: "content", value: page.content}),
+				}), 
+		});
+		if(error) {
+			console.log(error); 
+		}
+		done(html);
+		return; 
+	});
 }
